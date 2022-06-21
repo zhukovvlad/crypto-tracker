c993cbc73fd052805fc34f3dc48c74ffa56c84b9
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-22 00:12:22 +0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Select,
+  MenuItem,
+  makeStyles
+} from "@material-ui/core";
+
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flex: 1,
+        color: "gold",
+        fontFamily: "Montserrat",
+        fontWeight: "bold",
+        cursor: "pointer",
+    }
+}));
+
+const Header = () => {
+    const classes = useStyles();
+    const navigate = useNavigate();
+
+    return (
+      <AppBar color='transparent' position='static'>
+          <Container>
+              <Toolbar>
+                  <Typography onClick={() => navigate("/")} className={classes.title}>Crypto Tracker</Typography>
+                  <Select
+                      variant='outlined'
+                      style={{
+                          width: '100',
+                          height: '40',
+                          marginLeft: '15',
+                      }}
+                  >
+                      <MenuItem value={"USD"}>USD</MenuItem>
+                      <MenuItem value={"RUB"}>RUB</MenuItem>
+                  </Select>
+              </Toolbar>
+          </Container>
+      </AppBar>
+    )
+};
+
+export default Header
