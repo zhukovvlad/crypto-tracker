8d0c72c88cf872475fea4034893add926a8e50a0
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-23 21:08:22 +0300

    Created Title and Search Bar for coins

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -4,6 +4,14 @@ import { CoinList } from "../config/api";
 import axios from "axios";
 import { CryptoState } from "../CryptoContext";
 
+import {
+  createTheme,
+  ThemeProvider,
+  Container,
+  Typography,
+  TextField,
+} from "@material-ui/core";
+
 const CoinsTable = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -24,7 +32,32 @@ const CoinsTable = () => {
     fetchCoins();
   }, [currency]);
 
-  return <div>CoinsTable</div>;
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{ textAlign: "center" }}>
+        <Typography
+          variant="h4"
+          style={{ margin: 18, fontFamily: "Montserrat" }}
+        >
+          Cryptocurrency Prices by Market Cap
+        </Typography>
+        <TextField
+            label="Search For a Crypto Currency.."
+            variant="outlined"
+            style={{ marginBottom: 20, width: "100%" }}
+        />
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default CoinsTable;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  //<React.StrictMode>
     <CryptoContext>
       <App />
     </CryptoContext>
-  //</React.StrictMode>
 );
