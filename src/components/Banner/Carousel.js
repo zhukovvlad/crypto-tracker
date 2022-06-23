88719789a48e1cd027bcb4789553711e482cfbd7
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-23 16:29:34 +0300

    Started CoinsTable Component

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -23,7 +23,7 @@ const useStyles = makeStyles(() => ({
 }));
 
 export function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 const Carousel = () => {
@@ -43,44 +43,47 @@ const Carousel = () => {
     fetchTrendingCoins();
   }, [currency]);
 
-  const items = trending.map(coin => {
-    let profit = coin.price_change_percentage_24h >=0;
+  const items = trending.map((coin) => {
+    let profit = coin.price_change_percentage_24h >= 0;
     return (
-        <Link className={classes.carouselItem} to={`/coins/${coin.name}`}>
-            <img
-                src={coin?.image}
-                alt={coin.name}
-                height="80"
-                style={{ marginBottom: 10 }}
-            />
-            <span>{coin?.symbol}
-                &nbsp;
-                <span style={{
-                    color: profit > 0 ? "rgb(14, 203, 129)" : "red",
-                    fontWeight: 500,
-                }}
-                >
-                    {profit && "+"} {coin?.price_change_percentage_24h?.toFixed(2)}%
-                </span>
-            </span>
-            <span style={{ fontSize: 22, fontWeight: 500 }}>
-                {symbol} {numberWithCommas(coin?.current_price?.toFixed(2))}
-            </span>
-        </Link>
-    )
-  })
+      <Link className={classes.carouselItem} to={`/coins/${coin.name}`}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span
+            style={{
+              color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+              fontWeight: 500,
+            }}
+          >
+            {profit && "+"} {coin?.price_change_percentage_24h?.toFixed(2)}%
+          </span>
+        </span>
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol} {numberWithCommas(coin?.current_price?.toFixed(2))}
+        </span>
+      </Link>
+    );
+  });
 
   const responsive = {
     0: {
-        items: 2,
+      items: 2,
     },
     512: {
-        items: 4,
+      items: 4,
     },
   };
 
-  return <div className={classes.carousel}>
-    <AliceCarousel
+  return (
+    <div className={classes.carousel}>
+      <AliceCarousel
         mouseTracking
         infinite
         autoPlayInterval={1000}
@@ -90,8 +93,9 @@ const Carousel = () => {
         responsive={responsive}
         autoPlay
         items={items}
-    />
-  </div>;
+      />
+    </div>
+  );
 };
 
 export default Carousel;
