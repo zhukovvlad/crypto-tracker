673412f233b1ea071e3621bb98c64e84ed6fd6c2
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-26 16:52:30 +0300

    Installed buffer for correct html rendering

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { CryptoState } from "../CryptoContext";
 import { SingleCoin } from "../config/api";
+import ReactHtmlParser from "react-html-parser";
 
 import axios from "axios";
 import { makeStyles, Typography } from "@material-ui/core";
@@ -56,6 +57,14 @@ const CoinPage = () => {
       fontWeight: "bold",
       marginBottom: 20,
       fontFamily: "Montserrat",
+    },
+    description: {
+      width: "100%",
+      fontFamily: "Montserrat",
+      padding: 25,
+      paddingBottom: 15,
+      paddingTop: 0,
+      textAlign: "justify",
     }
   }));
 
@@ -73,6 +82,25 @@ const CoinPage = () => {
         <Typography variant="h3" className={classes.heading}>
           {coin?.name}
         </Typography>
+        <Typography variant="subtitle1" className={classes.description}>
+          {ReactHtmlParser(coin?.description.en.split(". ")[0])}.
+        </Typography>
+        <div className={classes.marketData}>
+          <span style={{ display: "flex" }}>
+            <Typography variant="h5" className={classes.heading}>
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Montserrat",
+              }}
+            >
+              {coin?.market_cap_rank}
+            </Typography>
+          </span>
+        </div>
       </div>
       <CoinInfo coin={coin} />
     </div>
