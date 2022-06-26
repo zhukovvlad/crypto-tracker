34923ad6b2a55e1ff262ce09be37d1a991d8ec21
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-26 16:37:03 +0300

    Installed React-html-parser

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -4,26 +4,79 @@ import { CryptoState } from "../CryptoContext";
 import { SingleCoin } from "../config/api";
 
 import axios from "axios";
+import { makeStyles, Typography } from "@material-ui/core";
+
+import CoinInfo from "../components/CoinInfo";
 
 const CoinPage = () => {
   const { id } = useParams();
+  console.log(id);
   const [coin, setCoin] = useState();
 
   const { currency, symbol } = CryptoState();
 
-  const fetchCoin = async () => {
-    const { data } = await axios.get(SingleCoin(id));
-
-    setCoin(data);
-  };
-
-  console.log(coin);
+  // const fetchCoin = async () => {
+  //   const { data } = await axios.get(SingleCoin(id));
+// 
+  //   setCoin(data);
+  // };
+// 
+  // console.log(coin);
 
   useEffect(() => {
-    fetchCoin();
-  }, [currency])
+    const fetchCoin = async () => {
+      const { data } = await axios.get(SingleCoin(id));
+  
+      setCoin(data);
+    };
 
-  return <div>CoinPage</div>;
+    fetchCoin();
+  }, [currency, id]);
+
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      display: "flex",
+      [theme.breakpoints.down("md")]: {
+        flexDirection: "column",
+        alignItems: "center",
+      }
+    },
+    sidebar: {
+      width: "30%",
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+      },
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      marginTop: 25,
+      borderRight: "2px solid grey",
+    },
+    heading: {
+      fontWeight: "bold",
+      marginBottom: 20,
+      fontFamily: "Montserrat",
+    }
+  }));
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.sidebar}>
+        <img
+          src={coin?.image.large}
+          alt={coin?.name}
+          height="200"
+          style={{ marginBottom: 20 }}
+        />
+        <Typography variant="h3" className={classes.heading}>
+          {coin?.name}
+        </Typography>
+      </div>
+      <CoinInfo coin={coin} />
+    </div>
+  );
 };
 
 export default CoinPage;
