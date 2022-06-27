66b19307d47b3e683dcdb68748e35a5c590dda98
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-27 16:51:21 +0300

    Initiated charts in CoinInfo Component

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { HistoricalChart } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import axios from "axios";
 
-const CoinInfo = () => {
-  return <div>CoinInfo</div>;
+import { createTheme, makeStyles, ThemeProvider } from "@material-ui/core";
+
+const CoinInfo = ({ coin }) => {
+  const [historicalData, setHistoricalData] = useState();
+  const [days, setDays] = useState(1);
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchHistoricalData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+
+    setHistoricalData(data.prices);
+  };
+
+  useEffect(() => {
+    fetchHistoricalData();
+  }, [currency, days]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  const useStyles = makeStyles(() => ({}));
+
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.container}></div>
+    </ThemeProvider>
+  );
 };
 
 export default CoinInfo;
