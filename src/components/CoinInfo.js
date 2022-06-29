375e0164170b4d0d46c8890734f321f33abc269f
Zhukovvlad <zhukovvlad@yandex.ru>
2022-06-29 14:14:19 +0300

    Added LineChart for coins

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -3,7 +3,35 @@ import { HistoricalChart } from "../config/api";
 import { CryptoState } from "../CryptoContext";
 import axios from "axios";
 
-import { createTheme, makeStyles, ThemeProvider } from "@material-ui/core";
+import {
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+  CircularProgress,
+} from "@material-ui/core";
+import { Line } from "react-chartjs-2";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const CoinInfo = ({ coin }) => {
   const [historicalData, setHistoricalData] = useState();
@@ -45,14 +73,51 @@ const CoinInfo = ({ coin }) => {
         padding: 20,
         paddingTop: 0,
       },
-    }
+    },
   }));
 
   const classes = useStyles();
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <div className={classes.container}></div>
+      <div className={classes.container}>
+        {!historicalData ? (
+          <CircularProgress
+            style={{ color: "gold" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicalData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
+
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+                datasets: [
+                  {
+                    data: historicalData.map((coin) => coin[1]),
+                    label: `Price (Past ${days} Days) in ${currency}`,
+                    borderColor: "#EEBC1D",
+                  }],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            />
+          </>
+        )}
+      </div>
     </ThemeProvider>
   );
 };
